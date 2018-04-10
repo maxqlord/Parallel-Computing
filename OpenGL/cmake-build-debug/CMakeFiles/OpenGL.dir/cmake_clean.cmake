6cedfd289ed3968da9211e9f45a74310d2b21dbd
maxqlord <lordmaxq@gmail.com>
2018-04-10 09:01:20 -0400

    Added openGL forest fire demo

---
+++ OpenGL/cmake-build-debug/CMakeFiles/OpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL.dir/main.c.o"
+  "OpenGL.pdb"
+  "OpenGL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
